0d174a14107858df9e6bb23760fcb717ce08e4df
Dikook <vimvom931@gmail.com>
2024-06-13 14:28:11 +0500

    Initial Commit

---
+++ RealAppProjectWithUikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealAppProjectWithUikit
+//
+//  Created by Diar Orynbek on 13.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
