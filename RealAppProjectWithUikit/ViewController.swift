1e857f214faf92dd576f4c1411d833c8ec852a23
Dikook <vimvom931@gmail.com>
2024-06-14 18:52:21 +0500

    today work

--- RealAppProjectWithUikit/ViewController.swift
+++ RealAppProjectWithUikit/ViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var babeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        let myLaptop = Laptop()
-        myLaptop.addChargeCycle()
+        babeLabel.text = "Hello, World@"
+        babeLabel.textColor = .blue
         
+        button.backgroundColor = .green
+        button.clipsToBounds = true
+        button.setTitle("Touch me", for: .normal)
     }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        babeLabel.textColor = getRandomColor()
+    }
+    
+    func getRandomColor() -> UIColor {
+         // Generate between 0 to 1
+         let red: CGFloat = CGFloat(drand48())
+         let green: CGFloat = CGFloat(drand48())
+         let blue: CGFloat = CGFloat(drand48())
 
+         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
