2fab1f95661f8956b4083b4949a28229ea445cff
Dikook <vimvom931@gmail.com>
2024-06-19 16:46:59 +0500

    today i did it. with snapkit. And add workspace
    /Users/diarorynbek/Desktop/Screenshot 2024-06-19 at 16.45.42.png

--- RealAppProjectWithUikit/ViewController.swift
+++ RealAppProjectWithUikit/ViewController.swift
@@ -11,26 +11,33 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Outlets
-    private lazy var label: UILabel = {
-        let label = UILabel()
-        label.text = "Hello, World"
-        label.textAlignment = .center
-        label.font = UIFont.boldSystemFont(ofSize: 25)
-        
-        return label
+    lazy var redSView = UIView(frame: .zero)
+    lazy var greenSView = UIView(frame: .zero)
+    lazy var blueSView = UIView(frame: .zero)
+    lazy var yellowSView = UIView(frame: .zero)
+    lazy var orangeSView = UIView(frame: .zero)
+    let boxWidth: CGFloat = 100
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "sec")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
     }()
     
     private lazy var blackButton: UIButton = {
         let button = UIButton()
+        button.backgroundColor = .gray
         button.setTitle("Круг", for: .normal)
-        
+        button.layer.cornerRadius = 50
         return button
     }()
     
     private lazy var greenButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Старт", for: .focused)
-        
+        button.backgroundColor = .green
+        button.setTitle("Старт", for: .normal)
+        button.layer.cornerRadius = 50
         return button
     }()
     
@@ -45,22 +52,41 @@ class ViewController: UIViewController {
     
     // MARK: - SetupView
     private func setupView() {
+        view.backgroundColor = .black
+        
+        redSView.backgroundColor = .red
+        greenSView.backgroundColor = .green
+        blueSView.backgroundColor = .blue
+        yellowSView.backgroundColor = .yellow
+        orangeSView.backgroundColor = .orange
+        
+        [redSView, greenSView, blueSView, yellowSView, orangeSView].forEach { box in
+            view.addSubview(box)
+        }
+        
         view.addSubview(blackButton)
         view.addSubview(greenButton)
+        view.addSubview(imageView)
     }
     
     // MARK: - SetupLayout
     private func setupLayout() {
-        blackButton.snp.makeConstraints{ make in
-            make.width.equalTo(300)
-            make.height.equalTo(100)
-            make.center.equalToSuperview()
+        imageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.top.equalTo(-50)
         }
         
-        blackButton.snp.makeConstraints{ make in
-            make.width.equalTo(300)
-            make.height.equalTo(100)
-            make.center.equalToSuperview()
+        blackButton.snp.makeConstraints { make in
+            make.width.height.equalTo(boxWidth)
+            make.centerY.leading.equalTo(40)
+            make.top.equalTo(476)
+        }
+
+        greenButton.snp.makeConstraints { make in
+            make.width.height.equalTo(boxWidth)
+            make.centerY.right.equalTo(-40)
+            make.top.equalTo(476)
         }
     }
 }
