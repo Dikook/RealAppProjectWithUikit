d3b22298a71e46ff6fd635e205fc4487c3611788
Dikook <vimvom931@gmail.com>
2024-06-27 02:04:30 +0500

    added tabbar to view.bacgorund = .white

--- RealAppProjectWithUikit/SceneDelegate.swift
+++ RealAppProjectWithUikit/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
+        let viewController = MainTabBarViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
