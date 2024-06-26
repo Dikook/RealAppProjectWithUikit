1fbf568c0c2cb289c034d2db9b61f0d8dbc2bc69
Dikook <vimvom931@gmail.com>
2024-06-27 01:58:14 +0500

    commit

---
+++ RealAppProjectWithUikit/MainTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealAppProjectWithUikit
+//
+//  Created by Diar Orynbek on 13.06.2024.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+    }
+    
+}
