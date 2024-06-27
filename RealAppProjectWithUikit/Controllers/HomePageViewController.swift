c432deb71126733118b5509af69f8de7b6b0ff6e
Dikook <vimvom931@gmail.com>
2024-06-28 03:12:11 +0500

    added personal cell. It is need to fix it

--- RealAppProjectWithUikit/Controllers/HomePageViewController.swift
+++ RealAppProjectWithUikit/Controllers/HomePageViewController.swift
@@ -9,10 +9,45 @@ import UIKit
 
 class HomePageViewController: UIViewController {
 
+    private let homeFeedTable: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+}
 
+extension HomePageViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = homeFeedTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hi Mr Bravon"
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        40
+    }
 }
---
+++ RealAppProjectWithUikit/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewTableViewCell.swift
+//  RealAppProjectWithUikit
+//
+//  Created by Diar Orynbek on 28.06.2024.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    static let identifier = "CollectionViewTableViewCell"
+    
+}
